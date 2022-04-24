bda2c0db456601a5ffed9c805f3c5e3dc26a2975
Preet987 <104312521+Preet987@users.noreply.github.com>
2022-04-24 20:36:42 +0530

    Uploaded Project Files

---
+++ css/resp.js
@@ -0,0 +1,18 @@
+
+function myfunctionz(){
+var burger=document.querySelector(".burger");
+ var navbar=document.querySelector(".navbar");
+ var rightnac=document.querySelector(".rightnac");
+ var navList=document.querySelector(".nav-list");
+
+
+
+
+
+
+    rightnac.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+    
+
+}
